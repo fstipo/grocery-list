90d3acfc645d8168ba067feec12e52edddd7b9a6
franko <fstipanov@gmail.com>
2023-05-22 19:30:10 +0200

    get and set functions - local storage

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,37 @@ import Form from "./Form";
 import { nanoid } from 'nanoid'
 import Items from "./Items";
 
+// Get locale storage
+const getLocaleStorage = () => {
+  let list = localStorage.getItem("my-list");
+  if (list) {
+    list = JSON.parse(list);
+  } else {
+    list = [];
+  }
+
+  return list;
+}
+const defaultList = JSON.parse(localStorage.getItem("my-list") || "[]");
+
+const setLocalStorage = (items) => localStorage.setItem("my-list", JSON.stringify(items));
+
 const App = () => {
-  const [items, setItems] = useState([]);
+  // const [items, setItems] = useState(getLocaleStorage());
+  const [items, setItems] = useState(defaultList);
+
+
   const addItem = (itemName) => {
     const newItem = { id: nanoid(), completed: false, name: itemName };
-    setItems([...items, newItem])
+    const newItems = [...items, newItem]
+    setItems(newItems);
+    setLocalStorage(newItems);
   }
 
   const removeItem = (itemId) => {
     let newItems = items.filter(item => itemId !== item.id);
     setItems(newItems);
+    setLocalStorage(newItems);
   };
   return (
     <section className="section-center">
